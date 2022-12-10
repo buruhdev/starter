6511e9fe7f2fe3eb8b12fae7a55604a2b658f159
aziyan99 <rajaazian08@gmail.com>
2022-12-10 21:08:09 +0700

    remove unused configs

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,14 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue'
-import path from 'path';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-    resolve: {
-        alias: {
-          '~coreui': path.resolve(__dirname, 'node_modules/@coreui/coreui')
-        }
-    },
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/admin.ts',
-                'resources/scss/admin.scss'
+                "resources/css/app.css",
+                "resources/js/admin.ts",
+                "resources/scss/admin.scss",
             ],
             refresh: true,
         }),
