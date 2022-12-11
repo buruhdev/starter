2e8291d8677c9ef4dd666ec29dadf4810949cd14
aziyan99 <rajaazian08@gmail.com>
2022-12-11 12:30:14 +0700

    splitting breadcump data

---
+++ resources/js/admin/components/Bottom.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <footer class="footer">
+        <div>
+            © 2022
+        </div>
+    </footer>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: "Bottom"
+})
+</script>
+
---
+++ resources/js/admin/components/SideMenu.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
+        <div class="sidebar-brand d-none d-md-flex">
+            <h4 class="sidebar-brand-full">
+                Name
+            </h4>
+            <h5 class="sidebar-brand-narrow">
+                N
+            </h5>
+        </div>
+        <ul class="sidebar-nav" data-coreui="navigation" data-simplebar="">
+            <li class="nav-item">
+                <a class="nav-link" href="index.html">
+                    <svg class="nav-icon">
+                        <use xlink:href="/coreui/vendors/@coreui/icons/svg/free.svg#cil-speedometer"></use>
+                    </svg> Dashboard
+                </a>
+            </li>
+        </ul>
+        <button class="sidebar-toggler" type="button" data-coreui-toggle="unfoldable"></button>
+    </div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: "Side Menu"
+})
+</script>
+
